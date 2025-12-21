8350fcab73102675c7f9aedcfe60a837305ecdbd
TheGauravsahu <itsgauravsahu7@gmail.com>
2025-12-21 16:54:15 +0530

    feat : add favicon.ico

--- src/components/dashboard/sidebar/index.tsx
+++ src/components/dashboard/sidebar/index.tsx
@@ -6,7 +6,7 @@ export default function Sidebar() {
     <div className="w-full h-screen  p-6">
       <Link to="/dashboard">
         <div className="w-full flex lg:items-center   gap-1">
-          <img src="/vite.svg" className="w-6 h-6" />
+          <img src="/favicon.ico" className="w-6 h-6" />
           <h1 className="font-semibold text-sm lg:text-[1rem]  hidden md:block">
             Learnify - Admin
           </h1>
